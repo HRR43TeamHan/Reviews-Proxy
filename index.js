cd576868b3f546a937f6952f4c0e0c9408e06378
Jordan Heskett <jordanheskett@gmail.com>
2020-02-08 08:22:16 -0600

    production bundle for pagespeed test

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const static = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/92139872d0.js" crossorigin="anonymous" ></script>
     <title>HRR43 FEC TeamHan!</title>
   </head>
   <body>
