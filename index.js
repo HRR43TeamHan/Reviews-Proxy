5a163a2aa3e8cfeefac2ff1b890379162893b3c0
Jordan Heskett <jordanheskett@gmail.com>
2020-02-08 18:19:48 -0600

    Trying to access module css

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const static = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://file.myfontastic.com/U8sGaLdnvhHsK7t6kuKpPn/icons.css" rel="stylesheet" />
+    <link href="http:/${BOOKING_HOSTNAME}:${BOOKING_PORT}/0/styles/style.css" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/4723f324c2.js" crossorigin="anonymous"></script>
     <title>HRR43 FEC TeamHan!</title>
     </head>
